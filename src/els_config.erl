674cf765ab686f28a8d9b62628b086b290dad834
Liam McNamara <liamjjmcnamara@gmail.com>
2020-03-29 20:09:23 +0200

    [6] add basic rebar3 parsing

--- src/els_config.erl
+++ src/els_config.erl
@@ -1,7 +1,7 @@
 -module(els_config).
 
 %% API
--export([ do_initialize/3
+-export([ do_initialize/4
         , initialize/3
         , get/1
         , set/2
@@ -23,9 +23,10 @@
 %% Macros
 %%==============================================================================
 -define(DEFAULT_CONFIG_FILE, "erlang_ls.config").
+-define(REBAR_CONFIG_FILE, "rebar.config").
 -define( DEFAULT_EXCLUDED_OTP_APPS
-       , [ "megaco"
-         , "diameter"
+       , [ "diameter"
+         , "megaco"
          , "snmp"
          , "wx"
          ]
@@ -37,35 +38,39 @@
                | apps_paths
                | capabilities
                | code_lenses
-               | diagnostics
+               | code_reload
                | deps_dirs
                | deps_paths
+               | diagnostics
                | include_dirs
                | include_paths
+               | otp_apps_exclude
                | otp_path
                | otp_paths
-               | otp_apps_exclude
                | plt_path
+               | project_type
+               | rebar_config
                | root_uri
-               | search_paths
-               | code_reload.
+               | search_paths.
 
 -type path()  :: file:filename().
 -type state() :: #{ apps_dirs        => [path()]
                   , apps_paths       => [path()]
                   , code_lenses      => [els_code_lens:lens_id()]
-                  , diagnostics      => [els_diagnostics:diagnostic_id()]
+                  , code_reload      => map() | 'disabled'
                   , deps_dirs        => [path()]
                   , deps_paths       => [path()]
+                  , diagnostics      => [els_diagnostics:diagnostic_id()]
                   , include_dirs     => [path()]
                   , include_paths    => [path()]
+                  , otp_apps_exclude => [string()]
                   , otp_path         => path()
                   , otp_paths        => [path()]
-                  , otp_apps_exclude => [string()]
                   , plt_path         => path()
+                  , project_type     => undefined | rebar
+                  , rebar_config     => map() | 'disabled'
                   , root_uri         => uri()
                   , search_paths     => [path()]
-                  , code_reload      => map() | 'disabled'
                   }.
 
 %%==============================================================================
@@ -75,14 +80,18 @@
 -spec initialize(uri(), map(), map()) -> ok.
 initialize(RootUri, Capabilities, InitOptions) ->
   RootPath = els_utils:to_list(els_uri:path(RootUri)),
-  Config = consult_config(config_paths(RootPath, InitOptions)),
-  do_initialize(RootUri, Capabilities, Config).
+  ElsConfig = consult_config(config_paths(RootPath, InitOptions)),
+  RebarConfig = maybe_rebar_config(RootPath),
+  do_initialize(RootUri, Capabilities, ElsConfig, RebarConfig).
 
--spec do_initialize(uri(), map(), {undefined|path(), map()}) -> ok.
-do_initialize(RootUri, Capabilities, {ConfigPath, Config}) ->
-  RootPath        = els_utils:to_list(els_uri:path(RootUri)),
+-spec do_initialize(uri(), map(), {undefined|path(), map()}, map()) -> ok.
+do_initialize(RootUri, Capabilities, {ConfigPath, Config}, RebarConfig) ->
+  RootPath = els_utils:to_list(els_uri:path(RootUri)),
+
+  % Allow override of rebar.confg
+  DepsDirs = resolve_config(deps_dirs, Config, RebarConfig),
+
   OtpPath         = maps:get("otp_path", Config, code:root_dir()),
-  DepsDirs        = maps:get("deps_dirs", Config, []),
   AppsDirs        = maps:get("apps_dirs", Config, ["."]),
   IncludeDirs     = maps:get("include_dirs", Config, ["include"]),
   Macros          = maps:get("macros", Config, []),
@@ -100,23 +109,24 @@ do_initialize(RootUri, Capabilities, {ConfigPath, Config}) ->
   CodeReload = maps:get("code_reload", Config, disabled),
 
   %% Passed by the LSP client
-  ok = set(root_uri       , RootUri),
+  ok = set(root_uri     , RootUri),
+  ok = set(capabilities , Capabilities),
   %% Read from the erlang_ls.config file
-  ok = set(config_path    , ConfigPath),
-  ok = set(otp_path       , OtpPath),
-  ok = set(deps_dirs      , DepsDirs),
-  ok = set(apps_dirs      , AppsDirs),
-  ok = set(include_dirs   , IncludeDirs),
-  ok = set(macros         , Macros),
-  ok = set(plt_path       , DialyzerPltPath),
-  ok = set(code_reload    , CodeReload),
+  ok = set(config_path  , ConfigPath),
+  ok = set(otp_path     , OtpPath),
+  ok = set(deps_dirs    , DepsDirs),
+  ok = set(apps_dirs    , AppsDirs),
+  ok = set(include_dirs , IncludeDirs),
+  ok = set(macros       , Macros),
+  ok = set(plt_path     , DialyzerPltPath),
+  ok = set(code_reload  , CodeReload),
   %% Calculated from the above
-  ok = set(apps_paths     , project_paths(RootPath, AppsDirs, false)),
-  ok = set(deps_paths     , project_paths(RootPath, DepsDirs, false)),
-  ok = set(include_paths  , include_paths(RootPath, IncludeDirs, false)),
-  ok = set(otp_paths      , otp_paths(OtpPath, false) -- ExcludePaths),
-  ok = set(code_lenses    , CodeLenses),
-  ok = set(diagnostics    , Diagnostics),
+  ok = set(apps_paths   , project_paths(RootPath, AppsDirs, false)),
+  ok = set(deps_paths   , project_paths(RootPath, DepsDirs, false)),
+  ok = set(include_paths, include_paths(RootPath, IncludeDirs, false)),
+  ok = set(otp_paths    , otp_paths(OtpPath, false) -- ExcludePaths),
+  ok = set(code_lenses  , CodeLenses),
+  ok = set(diagnostics  , Diagnostics),
   %% All (including subdirs) paths used to search files with file:path_open/3
   ok = set( search_paths
           , lists:append([ project_paths(RootPath, AppsDirs, true)
@@ -125,8 +135,6 @@ do_initialize(RootUri, Capabilities, {ConfigPath, Config}) ->
                          , otp_paths(OtpPath, true)
                          ])
           ),
-  %% Init Options
-  ok = set(capabilities  , Capabilities),
   ok.
 
 -spec start_link() -> {ok, pid()}.
@@ -190,7 +198,7 @@ possible_config_paths(Path) ->
 -spec consult_config([path()]) -> {undefined|path(), map()}.
 consult_config([]) -> {undefined, #{}};
 consult_config([Path | Paths]) ->
-  lager:info("Reading config file. path=~p", [Path]),
+  lager:info("Reading erlang_ls config file. path=~p", [Path]),
   Options = [{map_node_format, map}],
   try yamerl:decode_file(Path, Options) of
       [] -> {Path, #{}};
@@ -230,3 +238,39 @@ otp_paths(OtpPath, Recursive) ->
                          , OtpPath
                          , Recursive
                          ).
+
+-spec maybe_rebar_config(path()) -> map().
+maybe_rebar_config(RootPath) ->
+  RebarConfigPath = filename:join([RootPath, ?REBAR_CONFIG_FILE]),
+  lager:info("Trying to read rebar.config file. path=~p", [RebarConfigPath]),
+  case filelib:is_regular(RebarConfigPath) of
+    true  -> parse_rebar_config(RebarConfigPath);
+    false -> #{}
+  end.
+
+-spec parse_rebar_config(path()) -> map().
+parse_rebar_config(RebarConfigPath) ->
+  set(project_type, rebar),
+  case file:consult(RebarConfigPath) of
+    {ok, RebarConfig} ->
+      extract_rebar_config(RebarConfig);
+    {error, Error} ->
+      lager:warning( "Could not consult file ~p: ~p", [RebarConfigPath, Error]),
+      #{}
+  end.
+
+-spec extract_rebar_config(list()) -> map().
+extract_rebar_config(RebarConfig) ->
+  lists:foldl(fun process_rebar_option/2, #{}, RebarConfig).
+
+-spec process_rebar_option(term(), map()) -> map().
+process_rebar_option({Key, Value}, Acc) ->
+  maps:update(Key, Value, Acc).
+
+-spec resolve_config(atom(), map(), map()) -> [path()].
+resolve_config(deps_dirs, Config, RebarConfig) ->
+  RebarDeps = case maps:get(deps_dir, RebarConfig, undefined) of
+    undefined -> [];
+    DepsDir -> [DepsDir]
+  end,
+  maps:get("deps_dirs", Config, RebarDeps).
